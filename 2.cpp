0df22e0985700cfb2af278f1c515241b9eb48fc9
nikunj63 <nikunjs6324@gmail.com>
2024-03-05 23:27:29 +0530

    2.cpp

---
+++ 2.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"enter the number"<<endl;
+    int a , b;
+    cin>>a>>b;
+    int c = a*b/a*b;
+    cout<<c<<endl;
+    return 0;
+}
