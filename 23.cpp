8da68f67c40069ac8045e9132eaec32f76534c26
nikunj63 <nikunjs6324@gmail.com>
2024-03-06 09:55:54 +0530

    files

---
+++ 23.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    double length ,width, area;
+    cout<<"Enter the length of rectangle : ";
+    cin>>length;
+    cout<<"enter width of rectangle: ";
+    cin>>width;
+    area = length*width;
+    cout<<"Area of rectangle is: "<<area<<endl;
+    return 0;
+}
